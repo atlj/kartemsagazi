ea50286e7cf5a8195089f16c187b3276ae639024
Burak <cccswagboyyolo@yahoo.com>
2021-11-06 21:04:48 +0300

    feat: add og metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,58 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=Work+Sans:wght@400;700&display=swap"
                     rel="stylesheet"
                 />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/res/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/res/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/res/favicon/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/res/favicon/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/res/favicon/safari-pinned-tab.svg"
+                    color="#5bbad5"
+                />
+                <link rel="shortcut icon" href="/res/favicon/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#00aba9" />
+                <meta
+                    name="msapplication-config"
+                    content="/res/favicon/browserconfig.xml"
+                />
+                <title>Emsa Gazi Kart</title>
+                <meta name="theme-color" content="#ffffff" />
+                <meta
+                    property="og:title"
+                    content="Emsa Gazi Kart"
+                    key="title"
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:description"
+                    content="EMSA Gazi kartının anlaşmalı olduğu yerler."
+                    key="description"
+                />
+                <meta
+                    property="og:image"
+                    content="https://res.cloudinary.com/aatlj/image/upload/c_scale,w_1175/v1636221404/emsa_gazi_logo_o1tlkf.png"
+                    key="image"
+                />
+                <meta
+                    property="url"
+                    content="https://kartemsagazi.vercel.app"
+                    key="url"
+                />
             </Head>
             <ApolloProvider client={client}>
                 <Provider store={store}>
