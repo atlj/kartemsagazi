ea67be480bc22edc3840014d452dd74edf01651e
Burak <cccswagboyyolo@yahoo.com>
2021-11-06 19:19:07 +0300

    feat: add searchbar functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
-import Card from "@components/Card";
+import { Card, SearchBar } from "@components";
 import client from "@services/graphql";
 import { getPlaces } from "@services/graphql/queries/place";
 import { GetServerSideProps } from "next";
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 type Props = {
     places: Place[];
 };
 
 const Home = ({ places }: Props) => {
-    console.log(places);
+    const [searchString, setSearchString] = useState<string | null>(null);
+
+    const data = useMemo<Place[]>(() => {
+        if (searchString === null) {
+            return places;
+        }
+        // search for searchstring in places name
+        return places.filter((place) => {
+            if (
+                place.location.district
+                    .toLowerCase()
+                    .includes(searchString.toLowerCase()) ||
+                place.name.toLowerCase().includes(searchString.toLowerCase())
+            ) {
+                return true;
+            }
+            return false;
+        });
+    }, [searchString, places]);
 
     return (
-        <div className="bg-dark-400 pt-10 pl-5 pr-5 min-h-screen">
-            <div className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3 lg:grid-cols-6 gap-3 gap-y-4">
-                {places.map((place) => (
+        <div className="bg-dark-400 pt-10 pl-5 pr-5 min-h-screen flex flex-col ">
+            <SearchBar onChange={setSearchString} />
+            <div className="grid grid-cols-2 mt-8 md:grid-cols-4 sm:grid-cols-3 lg:grid-cols-6 gap-3 gap-y-4">
+                {data.map((place) => (
                     <Card
                         className="w-auto h-60 md:max-w-xs"
                         {...{
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import * as Icons from "@components/icons";
+import cn from "classnames";
+import styles from "./index.module.css";
+
+type Props = {
+    onChange: (text: string) => void;
+    className?: string;
+};
+
+function SearchBar({ onChange, className }: Props) {
+    return (
+        <div
+            className={cn(
+                "bg-dark-500 flex items-center pt-3 pb-3 pl-3 pr-3 rounded-lg w-full max-w-lg  self-center ",
+                className,
+            )}
+        >
+            <Icons.Search className="text-white w-5 h-5" />
+            <div className={cn("flex-1 flex ml-2 ", styles.field)}>
+                <input
+                    onInput={(event) => {
+                        onChange(event.currentTarget.value);
+                    }}
+                    className="text-white text-sm flex-1 "
+                    type="text"
+                    placeholder="Mekan ara"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
